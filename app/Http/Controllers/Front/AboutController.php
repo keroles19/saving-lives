a3f4cb8d3e0e684d3397385c5e109fd9e93e9e1a
keroles19 <kerolesatef200@gmail.com>
2022-06-19 21:42:39 +0200

    modify home and about controller

--- app/Http/Controllers/Front/AboutController.php
+++ app/Http/Controllers/Front/AboutController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Hospital;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
   public function about(){
-      $hospitals = User::where('is_admin',0)->where('status','1')->with(['country'=>function ($query){
+      $hospitals = Hospital::where('status','1')->with(['country'=>function ($query){
           $query->select('id','country_name');
       }])->get();
       return view('front.about  ',compact('hospitals'));
--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use App\Models\Article;
+use App\Models\Hospital;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,9 +12,10 @@ class HomeController extends Controller
 {
    public function index(){
        $articles = Article::where('status','1')->latest()->take(6)->get();
-       $hospitals = User::where('is_admin',0)->where('status','1')->with(['country'=>function ($query){
+       $hospitals = Hospital::where('status','1')->with(['country'=>function ($query){
            $query->select('id','country_name');
        }])->get();
+
        return view('front.index',compact(['articles','hospitals']));
 
    }
