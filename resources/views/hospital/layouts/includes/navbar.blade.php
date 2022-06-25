c9983a725f46e1b52cfdbb4ee6cc4871dd58a5f8
keroles19 <kerolesatef200@gmail.com>
2022-06-25 16:04:03 +0200

    chaneg

--- resources/views/hospital/layouts/includes/navbar.blade.php
+++ resources/views/hospital/layouts/includes/navbar.blade.php
@@ -7,10 +7,13 @@
         <ul class="nav navbar-nav align-items-center ms-auto">
             <li class="nav-item dropdown dropdown-user"><a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <div class="user-nav d-sm-flex d-none">
-                        <span class="user-name fw-bolder">John Doe</span>
+                        <span class="user-name fw-bolder">
+                            {{auth()->user()->name}}
+                        </span>
                         <span class="user-status">Admin</span></div><span class="avatar">
-                        <img class="round" src="{{asset('app-assets/images/portrait/small/avatar-s-11.jpg')}}"
-                             alt="avatar" height="40" width="40"><span class="avatar-status-online"></span></span>
+                        <img class="round" src="{{asset('storage/'.auth()->user()->photo)}}"
+                             alt="avatar" height="40" width="40"><span class="avatar-status-online"></span>
+                    </span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user">
 
